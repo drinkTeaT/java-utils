bfa79db221d2db25acb5d7c8e17b51183bcf5939
liuweibin <liuweibin3@sfmail.sf-express.com>
2021-02-01 14:06:03 +0800

    提交sql文件，配置文件

---
+++ src/main/java/club/tacbin/utils/JavaUtilsApplication.java
@@ -0,0 +1,16 @@
+package club.tacbin.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tacbin
+ */
+@SpringBootApplication
+public class JavaUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/tacbin/utils/JavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package club.tacbin.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
